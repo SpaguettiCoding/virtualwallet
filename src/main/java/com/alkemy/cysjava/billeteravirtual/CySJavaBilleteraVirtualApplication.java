9bc92a325e34c4c4f9859bb551aa3d63876462ad
Igancio <juanirod94@gmail.com>
2023-07-10 12:09:19 -0300

    Second Commit

---
+++ src/main/java/com/alkemy/cysjava/billeteravirtual/CySJavaBilleteraVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.alkemy.cysjava.billeteravirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CySJavaBilleteraVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CySJavaBilleteraVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alkemy/cysjava/billeteravirtual/CySJavaBilleteraVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.cysjava.billeteravirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CySJavaBilleteraVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
