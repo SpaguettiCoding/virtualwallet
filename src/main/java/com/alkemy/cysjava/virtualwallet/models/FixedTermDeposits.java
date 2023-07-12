7978fab9e2aa43131ae7781a922cf4f8021403f3
Igancio <juanirod94@gmail.com>
2023-07-12 19:19:53 -0300

    Fixing table name

--- src/main/java/com/alkemy/cysjava/virtualwallet/models/FixedTermDeposits.java
+++ src/main/java/com/alkemy/cysjava/virtualwallet/models/FixedTermDeposits.java
@@ -14,7 +14,7 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name="creditCard")
+@Table(name="fixed_term_deposits")
 public class FixedTermDeposits {
 
     @Id
