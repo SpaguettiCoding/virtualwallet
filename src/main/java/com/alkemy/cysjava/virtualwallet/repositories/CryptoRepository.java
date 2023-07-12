ce69d4da32fb61443966ce6fd0250bbf0848e8c4
Igancio <juanirod94@gmail.com>
2023-07-12 19:41:49 -0300

    Creating Crypto Repository

---
+++ src/main/java/com/alkemy/cysjava/virtualwallet/repositories/CryptoRepository.java
@@ -0,0 +1,9 @@
+package com.alkemy.cysjava.virtualwallet.repositories;
+
+import com.alkemy.cysjava.virtualwallet.models.Crypto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CryptoRepository extends JpaRepository<Crypto, Integer> {
+}
