ee7d287c5e5fa8a40effb0f8d6d7f95e06937b4f
Igancio <juanirod94@gmail.com>
2023-07-12 19:45:56 -0300

    Creating FixedTermDeposits Repository

--- src/main/java/com/alkemy/cysjava/virtualwallet/repositories/FixedTermDepositsRepository.java
+++ src/main/java/com/alkemy/cysjava/virtualwallet/repositories/FixedTermDepositsRepository.java
@@ -1,2 +1,9 @@
-package com.alkemy.cysjava.virtualwallet.repositories;public class FixedTermDepositsRepository {
+package com.alkemy.cysjava.virtualwallet.repositories;
+
+import com.alkemy.cysjava.virtualwallet.models.FixedTermDeposits;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FixedTermDepositsRepository extends JpaRepository<FixedTermDeposits, Integer> {
 }
