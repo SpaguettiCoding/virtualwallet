136da82b0414d796222e5b19bd6246511b1e9cfd
Igancio <juanirod94@gmail.com>
2023-07-17 11:02:00 -0300

    Creacion scripts sql para insert de roles admin y user

--- assets/database-scripts.sql
+++ assets/database-scripts.sql
@@ -3,10 +3,14 @@ USE `virtualwallet` ;
 
 CREATE TABLE IF NOT EXISTS `virtualwallet`.`roles` (
   `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL DEFAULT NULL,
+  `description` VARCHAR(255) NOT NULL,
   `creation_date` DATETIME(6) NOT NULL,
   `update_date` DATETIME(6) NOT NULL,
-  `description` VARCHAR(255) NOT NULL,
-  `name` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`))
 ;
+
+--INSERTS de roles / SEED DATA
+INSERT INTO roles (name, description, creation_date, update_date) VALUES ("admin", "rol de administrador", NOW(), NOW());
+INSERT INTO roles (name, description, creation_date, update_date) VALUES ("user", "rol de administrador", NOW(), NOW());
 
