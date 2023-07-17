a8b91dc1e530e13beafdbccb13a6a4ded67feb25
Facundo Monsegur <monsegurfacundo@gmail.com>
2023-07-17 15:45:17 -0300

    Creating the model and repository of account

--- src/main/java/com/alkemy/cysjava/virtualwallet/repositories/AccountRepository.java
+++ src/main/java/com/alkemy/cysjava/virtualwallet/repositories/AccountRepository.java
@@ -2,6 +2,8 @@ package com.alkemy.cysjava.virtualwallet.repositories;
 
 import com.alkemy.cysjava.virtualwallet.models.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AccountRepository extends JpaRepository<Account, Integer> {
 }
