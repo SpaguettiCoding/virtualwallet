938437befee41dd65d1934ca647b485ea80d99e2
JuliD <jgdirocco@gmail.com>
2023-07-24 21:43:27 -0300

    Created FixedTermDeposit model and repository

---
+++ src/main/java/com/alkemy/cysjava/virtualwallet/models/FixedTermDeposit.java
@@ -0,0 +1,49 @@
+package com.alkemy.cysjava.virtualwallet.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.time.temporal.ChronoUnit;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="fixed_term_deposits")
+public class FixedTermDeposit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private double amount;
+
+    @ManyToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+
+    @NotNull
+    private double interest;
+
+    private Timestamp creationDate;
+
+    private Timestamp closingDate;
+
+    public void setClosingDate(Timestamp closingDate) {
+        Timestamp currentTimestamp = new Timestamp(System.currentTimeMillis());
+        Timestamp minimumClosingDate = Timestamp.from(currentTimestamp.toInstant().plus(30, ChronoUnit.DAYS));
+
+        if (closingDate == null  || closingDate.before(minimumClosingDate)) {
+            this.closingDate = minimumClosingDate;
+        } else {
+            this.closingDate = closingDate;
+        }
+    }
+}
--- src/main/java/com/alkemy/cysjava/virtualwallet/repositories/FixedTermDepositRepository.java
+++ src/main/java/com/alkemy/cysjava/virtualwallet/repositories/FixedTermDepositRepository.java
@@ -1,9 +1,9 @@
 package com.alkemy.cysjava.virtualwallet.repositories;
 
-import com.alkemy.cysjava.virtualwallet.models.FixedTermDeposits;
+import com.alkemy.cysjava.virtualwallet.models.FixedTermDeposit;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FixedTermDepositsRepository extends JpaRepository<FixedTermDeposits, Integer> {
+public interface FixedTermDepositRepository extends JpaRepository<FixedTermDeposit, Long> {
 }
