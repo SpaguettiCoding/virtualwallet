83218ea4bb4f5e4b522a636b679f7384be89e041
Igancio <juanirod94@gmail.com>
2023-07-26 17:33:25 -0300

    Added missing @Component annotation to TransactionMapper

--- src/main/java/com/alkemy/cysjava/virtualwallet/mappers/TransactionMapper.java
+++ src/main/java/com/alkemy/cysjava/virtualwallet/mappers/TransactionMapper.java
@@ -4,7 +4,9 @@ import com.alkemy.cysjava.virtualwallet.DTOs.TransactionDepositDTO;
 import com.alkemy.cysjava.virtualwallet.models.Transaction;
 import com.alkemy.cysjava.virtualwallet.service.AccountService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TransactionMapper {
     
     @Autowired
