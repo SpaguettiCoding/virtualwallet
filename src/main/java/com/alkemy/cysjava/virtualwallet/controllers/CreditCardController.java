7d8753559c01f0918f50df6f625da8a605e92b4e
Facundo Monsegur <monsegurfacundo@gmail.com>
2023-07-26 15:14:17 -0300

    Creation of endpoint to delete credit card

--- src/main/java/com/alkemy/cysjava/virtualwallet/controllers/CreditCardController.java
+++ src/main/java/com/alkemy/cysjava/virtualwallet/controllers/CreditCardController.java
@@ -1,15 +1,13 @@
 package com.alkemy.cysjava.virtualwallet.controllers;
 
 import com.alkemy.cysjava.virtualwallet.DTOs.CreditCardCreationDTO;
+import com.alkemy.cysjava.virtualwallet.exceptions.ResourceNotFoundException;
 import com.alkemy.cysjava.virtualwallet.models.CreditCard;
 import com.alkemy.cysjava.virtualwallet.service.CreditCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/credit-card")
@@ -26,5 +24,11 @@ public class CreditCardController {
     public ResponseEntity<CreditCard> createCreditCard(@RequestBody CreditCardCreationDTO creditCardDTO) {
         CreditCard createdCreditCard = creditCardService.createCreditCard(creditCardDTO);
         return new ResponseEntity<>(createdCreditCard, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteCreditCardById(@PathVariable("id") Long id) {
+            creditCardService.deleteCreditCardById(id);
+            return new ResponseEntity<>("Deletion of CreditCard with ID number " + id + " was successful.", HttpStatus.OK);
     }
 }
