6322882ebba052b21406d6a25806116ffdbd6336
Fernando Silvero <ferpolasebas@gmail.com>
2023-08-01 19:05:47 -0300

    fixes and refactorization made

--- src/main/java/com/alkemy/cysjava/virtualwallet/service/FixedTermDepositService.java
+++ src/main/java/com/alkemy/cysjava/virtualwallet/service/FixedTermDepositService.java
@@ -5,37 +5,39 @@ import com.alkemy.cysjava.virtualwallet.DTOs.FixedTermDepositDTO;
 import com.alkemy.cysjava.virtualwallet.DTOs.FixedTermDepositCreationSimulateDTO;
 import com.alkemy.cysjava.virtualwallet.DTOs.FixedTermDepositSimulateDTO;
 import com.alkemy.cysjava.virtualwallet.exceptions.BadRequestException;
-import com.alkemy.cysjava.virtualwallet.exceptions.ResourceNotFoundException;
 import com.alkemy.cysjava.virtualwallet.mappers.FixedTermDepositMapper;
 import com.alkemy.cysjava.virtualwallet.models.Account;
 import com.alkemy.cysjava.virtualwallet.models.FixedTermDeposit;
+import com.alkemy.cysjava.virtualwallet.models.Transaction;
 import com.alkemy.cysjava.virtualwallet.repositories.FixedTermDepositRepository;
+import com.alkemy.cysjava.virtualwallet.repositories.TransactionRepository;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.sql.Timestamp;
+import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.Optional;
 
 @Service
 public class FixedTermDepositService {
 
     private final FixedTermDepositRepository fixedTermDepositRepository;
+    private final TransactionRepository transactionRepository;
     private final FixedTermDepositMapper fixedTermDepositMapper;
     private final AccountService accountService;
 
     private final double DEFAULT_INTEREST = 0.5;
+    private static final double INTEREST_RATE = 0.005;
 
-    public FixedTermDepositService(FixedTermDepositRepository fixedTermDepositRepository, AccountService accountService, FixedTermDepositMapper fixedTermDepositMapper){
+    public FixedTermDepositService(FixedTermDepositRepository fixedTermDepositRepository, TransactionRepository transactionRepository, AccountService accountService, FixedTermDepositMapper fixedTermDepositMapper){
         this.fixedTermDepositRepository = fixedTermDepositRepository;
+        this.transactionRepository = transactionRepository;
         this.accountService = accountService;
         this.fixedTermDepositMapper = fixedTermDepositMapper;
     }
     public FixedTermDepositDTO addFixedDeposit(FixedTermDepositCreationDTO fixedTermDepositCreationDTO) {
-        Optional<Account> optionalAccount = accountService.findOne(fixedTermDepositCreationDTO.getAccountId());
-
-        if (!optionalAccount.isPresent()) {
-            throw new ResourceNotFoundException("Account not found with ID: " + fixedTermDepositCreationDTO.getAccountId());
-        }
+        Account account = accountService.findAccountById(fixedTermDepositCreationDTO.getAccountId());
 
         //Valido que la fecha de cierre no sea menor a 30 días
         if(isClosingDate30daysMinimum(fixedTermDepositCreationDTO.getClosingDate())){
@@ -44,7 +46,7 @@ public class FixedTermDepositService {
 
         FixedTermDeposit fixedTermDeposit = fixedTermDepositMapper.toFixedTermDeposit(fixedTermDepositCreationDTO);
 
-        /* Valido que el balance de la cuenta sea mayor a la cantidad que se quiere invertir en el deposito
+         //Valido que el balance de la cuenta sea mayor a la cantidad que se quiere invertir en el deposito
         double accountBalance = fixedTermDeposit.getAccount().getBalance();
         double depositAmount = fixedTermDepositCreationDTO.getAmount();
 
@@ -53,16 +55,22 @@ public class FixedTermDepositService {
         }else{
             fixedTermDeposit.getAccount().setBalance(accountBalance - depositAmount);
         }
-         */
-        double accountBalance = fixedTermDeposit.getAccount().getBalance();
-        double depositAmount = fixedTermDepositCreationDTO.getAmount();
 
         fixedTermDeposit.getAccount().setBalance(accountBalance - depositAmount);
 
         fixedTermDeposit.setCreationDate(new Timestamp(System.currentTimeMillis()));
         fixedTermDeposit.setClosingDate(fixedTermDepositCreationDTO.getClosingDate());
         fixedTermDeposit.setInterest(DEFAULT_INTEREST);
+
+        Transaction fixedTermtransaction = new Transaction();
+        fixedTermtransaction.setAmount(fixedTermDeposit.getAmount());
+        fixedTermtransaction.setTransactionType("Fixed-term");
+        fixedTermtransaction.setTransactionDate(new Timestamp(System.currentTimeMillis()));
+        fixedTermtransaction.setDescription("Fixed term deposit");
+        fixedTermtransaction.setAccount(account);
+
         fixedTermDepositRepository.save(fixedTermDeposit);
+        transactionRepository.save(fixedTermtransaction);
 
         return fixedTermDepositMapper.toFixedTermDepositDTO(fixedTermDeposit);
     }
@@ -77,39 +85,42 @@ public class FixedTermDepositService {
 
     public FixedTermDepositSimulateDTO simulateFixedTerm(FixedTermDepositCreationSimulateDTO fixedTermDepositCreationSimulateDTO) {
 
-        //--Valido que la fecha de cierre no sea menor a 30 días
-        if(isClosingDate30daysMinimum(fixedTermDepositCreationSimulateDTO.getClosingDate())){
-            throw new BadRequestException("Closing date must be a minimum of 30 days.");
-        }
+        validateClosingDate(fixedTermDepositCreationSimulateDTO.getClosingDate());
+        validateAmount(fixedTermDepositCreationSimulateDTO.getAmount());
 
         FixedTermDepositSimulateDTO fixedTermDepositDTO = new FixedTermDepositSimulateDTO();
-
-        if (fixedTermDepositCreationSimulateDTO.getAmount() > 0) {
-            fixedTermDepositDTO.setAmount(fixedTermDepositCreationSimulateDTO.getAmount());
-        } else {
-            throw new BadRequestException("amount must be greater than 0");
-        }
-
+        fixedTermDepositDTO.setAmount(fixedTermDepositCreationSimulateDTO.getAmount());
         fixedTermDepositDTO.setClosingDate(fixedTermDepositCreationSimulateDTO.getClosingDate());
+        fixedTermDepositDTO.setCreationDate(Timestamp.from(Instant.now()));
 
-        fixedTermDepositDTO.setCreationDate(new Timestamp(System.currentTimeMillis()));
+        long days = ChronoUnit.DAYS.between(fixedTermDepositDTO.getCreationDate().toInstant(), fixedTermDepositDTO.getClosingDate().toInstant());
 
-        //--calculo el numero de dias entre creationDate y closingDate
-        long days = (fixedTermDepositDTO.getClosingDate().getTime() - fixedTermDepositDTO.getCreationDate().getTime()) / (1000 * 60 * 60 * 24);
-        //System.out.println(days);
-
-        //--calculo el interes ganado(0.5% por dia)
-        double interest = 0.005;
-        double interestEarned = fixedTermDepositDTO.getAmount() * interest * days;
+        double interestEarned = calculateInterest(fixedTermDepositDTO.getAmount(), days);
         fixedTermDepositDTO.setInterestEarned(interestEarned);
 
-        //--Calculo el porcentaje de interes generado en los dias
-        //double interestGenerated = days * interest;
-        //System.out.println(interestGenerated);
-
-        //--Calculo el monto total
         double totalAmount = fixedTermDepositDTO.getAmount() + interestEarned;
         fixedTermDepositDTO.setTotalAmount(totalAmount);
+
         return fixedTermDepositDTO;
+    }
+
+    private void validateClosingDate(Timestamp closingDate) {
+        Instant currentTimestamp = Instant.now();
+        Instant minimumClosingDate = currentTimestamp.plus(30, ChronoUnit.DAYS);
+
+        if (closingDate == null || closingDate.toInstant().isBefore(minimumClosingDate)) {
+            throw new BadRequestException("Closing date must be a minimum of 30 days.");
+        }
+    }
+
+    private void validateAmount(double amount) {
+        if (amount <= 0) {
+            throw new BadRequestException("Amount must be greater than 0.");
+        }
+    }
+
+    private double calculateInterest(double amount, long days) {
+        BigDecimal interest = BigDecimal.valueOf(amount * INTEREST_RATE * days);
+        return interest.setScale(2, RoundingMode.HALF_UP).doubleValue();
     }
 }
