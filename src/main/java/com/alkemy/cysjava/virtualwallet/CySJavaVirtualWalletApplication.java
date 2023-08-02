06e4a79261a8e7e247b1ef42d78081b742a66952
Igancio <juanirod94@gmail.com>
2023-08-02 15:05:09 -0300

    Dependencia Swagger OpenAPI added to pom.xml

--- src/main/java/com/alkemy/cysjava/virtualwallet/CySJavaVirtualWalletApplication.java
+++ src/main/java/com/alkemy/cysjava/virtualwallet/CySJavaVirtualWalletApplication.java
@@ -1,36 +1,7 @@
 package com.alkemy.cysjava.virtualwallet;
 
-
-
-import com.alkemy.cysjava.virtualwallet.models.Account;
-
-import com.alkemy.cysjava.virtualwallet.models.Role;
-
-import com.alkemy.cysjava.virtualwallet.models.User;
-
-
-import com.alkemy.cysjava.virtualwallet.repositories.AccountRepository;
-import com.alkemy.cysjava.virtualwallet.repositories.RoleRepository;
-import com.alkemy.cysjava.virtualwallet.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.boot.SpringApplication;
-
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import org.springframework.context.ApplicationContext;
-
-
-
-import java.sql.Timestamp;
-
-import java.util.ArrayList;
-
-import java.util.List;
-
-import java.util.Optional;
-
-
 
 @SpringBootApplication
 
